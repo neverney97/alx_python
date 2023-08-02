df9430e3724d2d3c7b0b8f9ad832cb20a280037f
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-02 19:55:13 +0000

    Another commit

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+word = "Holberton"
+word_first_3 = print(word[0:3])
+word_last_2 = print(word[-2:])
+middle_word = print(word[1:-1])
+# print statments
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
