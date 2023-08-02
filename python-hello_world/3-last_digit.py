d09f753cad42934cdc44175fc934b3d9cb3719ba
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-02 20:37:54 +0000

    commit

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,11 @@
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+
+lastDigitOf = abs(number) % 10
+if lastDigitOf > 5:
+    print("Last digit of", number, "is", lastDigitOf, "and is greater than 5")
+elif lastDigitOf == 0:
+    print("Last digit of", number, "is", lastDigitOf, "and is 0")
+elif (lastDigitOf < 6) & (lastDigitOf != 0):
+    print("Last digit of", number, "is", lastDigitOf, "and is less than 6 and not 0")
