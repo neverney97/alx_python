44a31a9c05a5d3d70cf5ba32dff9ecc081504851
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-05 00:59:28 +0000

    w

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,12 +1,12 @@
 def validate_password(password):
     if len(password) == 8:
         return True
-    if password.isupper() == True:
-        return True
-    if password.islower() == True:
-        return True
-    elif password.isdigit() == True:
-        return True
+        if password.isupper() == True:
+            return True
+        if password.islower() == True:
+            return True
+        elif password.isdigit() == True:
+            return True
     else:
         return False
     
