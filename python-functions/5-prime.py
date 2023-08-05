f3d278443fabe91cb1ef5d51cfef41a011a6eac7
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-05 00:38:40 +0000

    1

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,7 @@
 def is_prime(number):
-    for i in range(2, number):
-        if number % i ==0:
-            return False
-        else:
-            return True
+  for i in range(2,int(number/2)):
+    if (number%i) == 0:
+      return False
+  return True
+
+print(is_prime(15))
