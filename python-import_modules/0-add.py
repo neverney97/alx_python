15e9d9ccfc7489fa49d891255d6eddbde0f6c0ee
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-10 20:19:31 +0000

    q

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,2 @@
 # import the add function from add_0
 from add_0 import add
-# calling the function
-add(1, 2)
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,4 +1,7 @@
 # defining the add function
 
 def add(a, b):
+    a = 1
+    b = 1
     print("{} + {} = {}".format(a, b, a + b))
+    
