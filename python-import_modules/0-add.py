5cbbdd734a8d3c8d54bcaf5659ef86d4657ad5be
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-10 20:06:57 +0000

    commit

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+# import the add function from add_0
+
+from add_0 import add
+
+# calling the function
+
+print(add(1, 2))
---
+++ python-import_modules/add_0.py
@@ -0,0 +1,4 @@
+# defining the add function
+
+def add(a, b):
+    print("{} + {} = {}".format(a, b, a + b))
