69ead3797dfe390e6fa894c914ff6c1e7aff3ce4
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 19:27:00 +0000

    a

---
+++ python-import_modules/3-main.py
@@ -0,0 +1,7 @@
+from 3-safe_print_division import safe_print_division
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division
+    
---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, d):
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        print("None")
+    finally:
+        print("{}: {}".format("Inside result", result))
+        print("{} / {} = {}".format(a, b, result))
