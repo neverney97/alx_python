5a17927c65f9e41e59deada73f9a9c13577cd984
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 19:32:12 +0000

    a

--- python-import_modules/3-main.py
+++ python-import_modules/3-main.py
@@ -1,7 +1,3 @@
-from 3-safe_print_division import safe_print_division
-
 if __name__ == "__main__":
-    a = 12
-    b = 2
-    result = safe_print_division
+    from safe_print_division import safe_print_division
     
