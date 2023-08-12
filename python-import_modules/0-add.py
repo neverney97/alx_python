bfea7d825011cf60d5675859a45c14a4982da543
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 16:26:22 +0000

    a

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,11 @@
+#!/usr/local/bin/python3
+
+a = 1
+b = 1
+result = a + b
+
 # import the add function from add_0
-from add_0 import add
-_main_ = __name__
+if __name__ == "__main__":
+    from add_0 import add
+    print("{} + {} = {}".format(a, b, result))
+
