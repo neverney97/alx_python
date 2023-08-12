d2d4e3d85feba07dfcaf6f0abd412b06dc97040b
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 18:48:27 +0000

    a

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,10 @@
 import sys
-
+print(sys.argv)
 def main():
     argv = sys.argv[1:]  # Exclude the script name from arguments
     num_args = len(argv)
 
-    print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args))
+    print("{}: {}".format(argv.index, num_args))
     print(":")
     
     for i, arg in enumerate(argv, start=1):
