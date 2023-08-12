9d7e1aa61bfd4424b8b4cd8b2ef9e1ca7deeeca1
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 17:18:21 +0000

    a

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,5 +1,6 @@
 # creating a list of the arguments
-argv = ["Hello", "Holberton", "School", 98, "Battery", "street"]
+if __name__ == "__main__":
+    argv = ["Hello", "Holberton", "School", 98, "Battery", "street"]
 
-for i, argv in enumerate(argv, start=1):
-    print("{}: {}".format(i, argv))
+    for i, argv in enumerate(argv, start=1):
+        print("{}: {}".format(i, argv))
