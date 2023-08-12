f308d538ecb8b8c977cf69c5a2ac227c1e07da5e
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 20:38:34 +0000

    a

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,8 +2,10 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None
+        pass
     finally:
+        if b == 0:
+            result = None
         print("Inside result: {}".format(result))
 
 
