57aee14da831e9a11e0808c0a3a672c428604c00
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 20:58:17 +0000

    a

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,5 +1,4 @@
-def raise_exception():
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception has been raised")
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception has been raised")
