65fb69fb262d1a747777f25c8af1705c02d6c3db
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-12 21:22:26 +0000

    a

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,10 +1,2 @@
 def raise_exception_msg(message):
     raise NameError()
-
-    try:
-        if message == "":
-            raise NameError
-    except NameError as ne:
-        print(ne)
-    else:
-        print(message)
