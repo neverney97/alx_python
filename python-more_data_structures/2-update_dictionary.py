ae5f965ba7806f100a69ae92bb880f69be8df32d
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-16 18:27:07 +0000

    b

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -3,3 +3,18 @@ def update_dictionary(a_dictionary, key: str, value):
         a_dictionary[key] = value
     else:
         a_dictionary[key] = value
+
+def print_sorted_dictionary(my_dict):
+    """ Print sorted dictionary """
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
+
+
+my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
+key = "a"
+value = "A"
+new_dict = update_dictionary(my_dict, key, value)
+print_sorted_dictionary(new_dict)
+print("xx")
+print_sorted_dictionary(my_dict)
