ad452172cea57ee278e2b404329ce14d05ddf458
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-19 23:54:23 +0000

    a

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -27,7 +27,7 @@ class Square:
                 raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = value
+            self.size = value
             
     def area(self):
         return self.size ** 2
