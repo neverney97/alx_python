efa764267d62d0c7834933bc0cdb7f28de5f0a32
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-19 23:39:26 +0000

    A

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
     It has an attribute of size
     """
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
         @property
         def size(self):
