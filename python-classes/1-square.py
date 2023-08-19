976178aa71d763b917e39136246fc955fca0573b
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-19 23:10:39 +0000

    a

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -20,9 +20,9 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size = size
-        
+
         if not isinstance:
             raise TypeError("size must be an integer")
         
-        if size < 0:
+        if int(size) < 0:
             raise ValueError("size must be >= 0")
