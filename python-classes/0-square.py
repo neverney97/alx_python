51355a9d4e1636ef47c268608c7cc3b4767ea396
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-19 22:53:55 +0000

    a

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
+"""
+Write a class Square that defines a square by:
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+"""
 class Square:
+    """
+    This class represents a square
+
+    The class has 1 attribute being the size and it is private, as represented using 2 underscores before the attribute name.
+    """
     def __init__(self, size=None):
         self.__size = size
