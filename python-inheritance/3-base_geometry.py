11abf7d7c98156ab37beaf52a4078e9376d043a9
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-24 01:41:32 +0000

    a

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,7 +4,7 @@ Write an empty class BaseGeometry.
 You are not allowed to import any module
 """
 
-class BaseGeometry():
+class BaseGeometry:
     """
     The name of the class is as above and it is empty for now.
     """
