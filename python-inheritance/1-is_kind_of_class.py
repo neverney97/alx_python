ae991be4748b1b704ea4ba5d01f430dcff00f103
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-24 01:34:06 +0000

    a

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,13 @@
+"""
+Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.
+
+Prototype: def is_kind_of_class(obj, a_class):
+You are not allowed to import any module
+
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    The name of the function is as above
+    """
+    return isinstance(obj, a_class)
