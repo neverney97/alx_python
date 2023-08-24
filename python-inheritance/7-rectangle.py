df0a9f31eabe2bcee39977370621d89de8ec1440
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-24 02:20:59 +0000

    a

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,44 @@
+"""
+Write a class BaseGeometry (based on 3-base_geometry.py).
+
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+You are not allowed to import any module
+"""
+class BaseGeometry:
+    """
+    This is the class' name and it has 2 public instance methods.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+"""
+Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
+
+Instantiation with width and height: def __init__(self, width, height):
+width and height must be private. No getter or setter
+width and height must be positive integers, validated by integer_validator
+"""
+
+class Rectangle(BaseGeometry):
+    """
+    This is a clas Rectangle that inherits from BaseGeometry
+
+
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
