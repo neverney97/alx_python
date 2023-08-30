fbcce16063a035d50266caa556ec5e563e6d8844
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-30 23:33:26 +0000

    a

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,8 +4,6 @@ Write a class BaseGeometry (based on 3-base_geometry.py).
 Public instance method: def area(self): that raises an Exception with the message area() is not implemented
 You are not allowed to import any module
 """
-from typing import Any
-
 
 class BaseGeometry:
     """
