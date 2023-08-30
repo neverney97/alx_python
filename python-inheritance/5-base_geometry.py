59c805eb128cff8cd8dc26b000103e663aa99101
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-30 23:33:03 +0000

    a

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,10 +4,15 @@ Write a class BaseGeometry (based on 3-base_geometry.py).
 Public instance method: def area(self): that raises an Exception with the message area() is not implemented
 You are not allowed to import any module
 """
+from typing import Any
+
+
 class BaseGeometry:
     """
     This is the class' name and it has 2 public instance methods.
     """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     def area(self):
         raise Exception("area() is not implemented")
     
