37a560686c9f928346e64366c9a30b977695f15c
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-30 23:23:36 +0000

    a

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,7 +4,20 @@ Write an empty class BaseGeometry.
 You are not allowed to import any module
 """
 
-class BaseGeometry:
+from collections.abc import Iterable
+
+
+class BaseMetaClass(type):
+    """
+    The class above represents a meta class for removing __init_subclass__ from the main class, BaseGeometry.
+    """
+    def __dir__(cls):
+        """
+        Method to remove __init_subclass__
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BaseGeometry(metaclass=BaseMetaClass):
     """
     The name of the class is as above and it is empty for now.
     """
@@ -13,3 +26,5 @@ class BaseGeometry:
         This is a method to remove __init_subclass__ from the dir list.
         """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+print(dir(BaseGeometry))
