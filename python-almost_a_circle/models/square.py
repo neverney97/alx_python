17594a1abd90cee57c433b58eddee0a0084a9ec9
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-31 01:58:42 +0000

    a

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
     """This class, Square, inherits from its superclass Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """ This is the __init__ method"""
-        super().__init__(size, size, x, y, id=None)
+        super().__init__(size, size, x, y, id)
         self.__height = size
         self.__width = size
         self.__size = size
