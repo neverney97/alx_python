07f2cd714cf9e2e087bf6dca925fb6b5b71ae40f
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-31 01:10:21 +0000

    a

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,7 +96,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
 
