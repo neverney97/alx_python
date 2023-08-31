0164863e239dd45421a9abc5c387b81d46e264ca
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-08-31 00:45:48 +0000

    a

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,14 @@ Because we want to protect attributes of our class. With a setter, you are able 
 """
 
 from base import Base
-# This is an import statement to import the class Base from the base python file
+""" This is an import statement to import the class Base from the base python file
+"""
 
 class Rectangle(Base):
     """
     This class represents the rectange class which is a child class of the Base class.
+
+
     """
     def __init__(self, width, height, x, y, id=None):
         super().__init__(id)
@@ -30,39 +33,55 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-    # Getter method for width
+    """ Getter method for width
+
+    """
     @property
     def width(self):
         return self.__width
     
-    # Setter method for width
+    """ Setter method for width
+
+    """
     @width.setter
     def width(self, value):
         self.__width = value
 
-    # getter method for height
+    """ getter method for height
+
+    """
     @property
     def height(self):
         return self.__height
     
-    # setter method for height
+    """ setter method for height
+
+    """
     @height.setter
     def height(self, value):
         self.__height = value
 
-    # getter method for x
+    """ getter method for x
+
+    """
     def x(self):
         return self.__x
     
-    # Setter method for x
+    """ Setter method for x
+
+    """
     def x(self, value):
         self.__x = value
 
-    #Getter method for y
+    """Getter method for y
+
+    """
     def y(self):
         return self.__y
     
-    #setter method for y
+    """setter method for y
+
+    """
     def y(self, value):
         self.__y = value
 
