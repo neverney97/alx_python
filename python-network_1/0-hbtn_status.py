1b2027923d2870597a3e55c2531fd486c934011f
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-09-05 23:44:12 +0000

    a

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,12 +5,10 @@ You must use the package requests
 You are not allow to import packages other than requests
 The body of the response must be display like the following example (tabulation before -)
 """
-
 import requests
 """
 This statement imports the requests module
 what does this module or library do? It is a library for making HTTP requests.
-
 """
 
 request = requests.get("https://alu-intranet.htbn.io/status")
