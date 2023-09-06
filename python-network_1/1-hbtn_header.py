a53e8b339b6cd33e1e0ead2db4c85f333c0c7bb3
Emmanuel Nani <86957173+neverney97@users.noreply.github.com>
2023-09-06 00:11:10 +0000

    a

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,21 @@
+"""
+Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+
+You must use the packages requests and sys
+You are not allow to import other packages than requests and sys
+The value of this variable is different for each request
+You don’t need to check script arguments (number and type)
+"""
+
+# Import the requests and sys packages
+import requests
+import sys
+
+# The url name
+url = 'https://alu-intranet.hbtn.io/status'
+
+# Send an HTTP GET request to the url
+req = requests.get(url)
+
+# Checking the value of the variable X-Request-Id
+print(req.headers['X-Request-Id'])
